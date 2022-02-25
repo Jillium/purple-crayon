01958ac8279adb6c1a2acb1dbf6ebc42b7ddab4b
Jillium <86424914+Jillium@users.noreply.github.com>
2022-02-25 13:51:19 -0700

    initial routing structure and added folders to file

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'; // v5
+
+import MyPic from './components/MyPic';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <div>
+        <header className='header'>
+
+          <h1 className='header-name'>Jill Holmes</h1>
+          <ul className='nav'>
+            <li className='nav-item'>
+              <Link to="/" style={{ textDecoration: 'none'}}>About Me</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="/portfolio" style={{ textDecoration: 'none'}}>Portfolio</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="/contact" style={{ textDecoration: 'none'}}>Contact Me</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="/resume" style={{ textDecoration: 'none'}}>Resume</Link>
+            </li>
+          </ul>
+        </header>
+
+
+
+
+      </div>
+      </Router>
+
+
+        <MyPic />
+    </div>
+  );
+}
+
+export default App;
