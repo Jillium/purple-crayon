4071dc50100a33298ae9c339381201b27dc78ce2
Jillium <86424914+Jillium@users.noreply.github.com>
2022-03-01 13:42:55 -0700

    still not working

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,12 +8,32 @@ import pic5 from "../assets/profilepics/pic5.JPG";
 import pic6 from "../assets/profilepics/pic6.JPG";
 
 function About() {
-
+    const pictureChoices = [pic2, pic3, pic4, pic5, pic6]
     const [chosenPic, setNewPic ] = useState(pic1);
-    const pictureChoices = [pic1, pic2, pic3, pic4, pic5, pic6]
     
+    let index = 0;
 
     function changePicture() {
+       
+        index++;
+        index %= pictureChoices.length;
+        console.log(pictureChoices[index]);
+
+        if (index < pictureChoices.length - 1) {
+            setNewPic(pictureChoices[index]);
+        }
+        
+
+
+        // let i = 1
+        // i++;
+        // console.log(pictureChoices[i]);
+
+        // while (i < pictureChoices.length) {
+        //     console.log(i);
+        //     console.log(pictureChoices[i]);
+        //     i++
+        // }
 
         // for (var i = 0; i < pictureChoices.length; i++) {
         //     console.log(pictureChoices[i]);
@@ -29,12 +49,12 @@ function About() {
         
 
         // var randomPicture = pictureChoices[index];
-        setNewPic(pic3);
+        // setNewPic(pictureChoices[i]);
         
     }
 
     return (
-        <div  onClick={changePicture}><img src={chosenPic} className="profile-pic" alt="profile images"></img></div>
+        <div onClick={changePicture}><img src={chosenPic} className="profile-pic" alt="profile images"></img></div>
     )
 }
 
